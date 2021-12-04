029e1b7bec1b80c6d1e013e620e3e31d993cdce6
ravi185patel <aryanrdp@gmail.com>
2021-12-04 10:53:32 +0530

    concepts new classes

---
+++ src/designpattern/MediatorDemo.java
@@ -0,0 +1,11 @@
+package designpattern;
+
+import java.util.*;
+public class MediatorDemo {
+    public static void main(String[] args) {
+        HashMap h=new HashMap();
+        System.out.println(h.size());
+        // h.size();
+        // Executors 
+    }
+}
